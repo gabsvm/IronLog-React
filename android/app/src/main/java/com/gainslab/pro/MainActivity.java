09522a177232c9f48259711b2b0c3e61d3e25505
gabsvm <53277787+gabsvm@users.noreply.github.com>
2026-08-12 18:59:28 -0300

    register native bridge plugin

--- android/app/src/main/java/com/gainslab/pro/MainActivity.java
+++ android/app/src/main/java/com/gainslab/pro/MainActivity.java
@@ -1,5 +1,13 @@
 package com.gainslab.pro;
 
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        registerPlugin(NativeBridgePlugin.class);
+    }
+}
